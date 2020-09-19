4281f3815768129d440495cdc0e5cf3ad2426592
raulgarcia <garcia.raul@gmail.com>
2020-09-19 01:21:53 -0400

    RxSwift, RxCocoa and RxRetroSwift dependencies added

---
+++ UrbanDictionary/UrbanDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UrbanDictionary
+//
+//  Created by Saulo Garcia on 9/18/20.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
